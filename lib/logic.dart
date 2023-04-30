f1dbfe33de02f503d60de0690e0fae874eba3c62
firlynankeceeeeeh <laodeoshama0001@gmail.com>
2023-04-30 19:46:41 +0700

    first ampt

---
+++ lib/logic.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+class Logic {
+  List<bool> firstLevel = [];
+  generateList() {
+    for (var i = 1; i <= 9; i++) {
+      Random random = Random();
+      int randomNumber = random.nextInt(10);
+      randomNumber > 5 ? firstLevel.add(true) : firstLevel.add(false);
+    }
+    return firstLevel;
+  }
+
+  checkGame(list) {
+    var count = 0;
+    var result = '';
+    list.map((item) {
+      item == true ? count++ : count = count;
+    }).toList();
+    count == 9
+        ? result = 'sukses'
+        : count == 0
+            ? result = 'gagal'
+            : result = '';
+    return result;
+  }
+
+  showEndScreen(context, message, steps) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Game Over'),
+            content: SizedBox(
+              height: 180,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(message),
+                  ElevatedButton(
+                      onPressed: () {
+                        Share.share(
+                            'Jumlah Langkah Game saya adalah $steps Langkah');
+                      },
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                        child: Text('Share You\'r Record'),
+                      ))
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
